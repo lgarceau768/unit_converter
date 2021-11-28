4fecb5e8610ecfa726c1248364edfe70910f167b
Luke Garceau <lgarceau@uncc.edu>
2021-11-28 13:12:59 -0500

    Updates from pc

--- main.py
+++ main.py
@@ -4,6 +4,44 @@
 # Flexion Code Challenge v3.2
 SIGNIFICANT_FIGS = 2
 
+## Units Variable
+UNITS = {
+    'temperature': {
+        'c': {
+            'val': 'celsius',
+        },
+        'k': {
+            'val': 'kelvin',
+        },
+        'f': {
+            'val': 'fahrenheit',
+        },
+        'r': {
+            'val': 'rankine',
+        }
+    },
+    'volume': {
+        'l': {
+            'val': 'liters',
+        },
+        'tbsp': {
+            'val': 'tablespoons',
+        },
+        'ci': {
+            'val': 'cubic-inches',
+        },
+        'cf': {
+            'val': 'cubic-feet',
+        },
+        'c': {
+            'val': 'cups',
+        },
+        'g': {
+            'val': 'gallons',
+        }
+    }
+}
+
 ## Helper Functions
 # Function to chop off extra decimals
 def prepNum(num):
@@ -37,10 +75,10 @@ def askInput(prompt, number=False, unit=False):
             line = line.lower().rstrip()
             validUnit = False
             for item in UNITS['volume']:
-                if item['val'] == line:
+                if UNITS['volume'][item]['val'] == line:
                     validUnit = True
             for item in UNITS['temperature']:
-                if item['val'] == line:
+                if UNITS['temperature'][item]['val'] == line:
                     validUnit = True
             if not validUnit:
                 print('Please enter a valid unit of measurement')
@@ -50,7 +88,37 @@ def askInput(prompt, number=False, unit=False):
         else:
             return line
 
-## Functions for conversions (using kelvin as a base)
+# Funciton to pull unit key from input
+def getUnit(inputVal):
+    refUnit = ''
+    temp = False
+    for unit in UNITS['temperature']:
+        if UNITS['temperature'][unit]['val'] == inputVal:
+            refUnit = unit
+            temp = True
+    for unit in UNITS['volume']:
+        if UNITS['volume'][unit]['val'] == inputVal:
+            refUnit = unit
+    if refUnit == '':
+        raise ValueError('Invalid unit input')
+    else:
+        if temp:
+            unitFamily = 'temperature'
+        else:
+            unitFamily = 'volume'
+        return [refUnit, unitFamily]
+
+# Function to validate student input
+def validateStudent(num, startUnit, targetUnit, studentInput):
+    # need to pull out the conversion function from the units table
+    [tUnit, unitFamily] = getUnit(targetUnit)
+    answerVal = UNITS[unitFamily][tUnit]['convert'](num, startUnit)
+    if answerVal == studentInput:
+        print('correct')
+    else:
+        print('incorrect')  
+
+## Functions for conversions (using kelvin and liters as a base)
 def kelvin(num, unit):
     num = float(num)
     returnVal = num
@@ -75,58 +143,70 @@ def celsius(num, unit):
 def rankine(num, unit):
     return prepNum(float(kelvin(num, unit)) * (9/5))
 
+def liters(num, unit):
+    num = float(num)
+    returnVal = num
+    if unit == UNITS['volume']['l']['val']:
+        returnVal = num
+    elif unit == UNITS['volume']['tbsp']['val']:
+        returnVal = (num / 67.63)
+    elif unit == UNITS['volume']['ci']['val']:
+        returnVal = (num * 0.016387)
+    elif unit == UNITS['volume']['cf']['val']:
+        returnVal = (num / 0.035315)
+    elif unit == UNITS['volume']['c']['val']:
+        returnVal = (num / 4.2268)
+    elif unit == UNITS['volume']['g']['val']:
+        returnVal = (num * 3.785)
+    else:
+        raise ValueError
+    return prepNum(returnVal)
 
+def tablespoons(num, unit):
+    return (float(liters(num, unit)) * 67.63)
 
-## Units Variable
-UNITS = {
-    'temperature': {
-        'c': {
-            'val': 'celsius',
-            'convert': celsius
-        },
-        'k': {
-            'val': 'kelvin',
-            'convert': kelvin
-        },
-        'f': {
-            'val': 'fahrenheit',
-            'convert': fahrenheit,
-        },
-        'r': {
-            'val': 'rankine',
-            'convert': rankine
-        }
-    },
-    'volume': {
-        'l': {
-            'val': 'liters',
-            'convert': ''
-        },
-        'tbsp': {
-            'val': 'tablespoons',
-            'convert': ''
-        },
-        'ci': {
-            'val': 'cubic-inches',
-            'convert': ''
-        },
-        'cf': {
-            'val': 'cubic-feet',
-            'convert': ''
-        },
-        'c': {
-            'val': 'cups',
-            'convert': ''
-        },
-        'g': {
-            'val': 'gallons',
-            'convert': ''
-        }
-    }
-}
+def cubicInches(num, unit):
+    return (float(liters(num, unit)) * 61.02)
+
+def cubicFeet(num, unit):
+    return (float(liters(num, unit)) * 0.035315)
+
+def cups(num, unit):
+    return (float(liters(num, unit)) * 4.2268)
+
+def gallons(num, unit):
+    return (float(liters(num, unit)) * 0.2641)
+
+# Attach all the conversion functions to the units variable
+for item in UNITS['temperature']:
+    if item == 'c':
+        UNITS['temperature'][item]['convert'] = celsius
+    if item == 'f':
+        UNITS['temperature'][item]['convert'] = fahrenheit
+    if item == 'r':
+        UNITS['temperature'][item]['convert'] = rankine
+    if item == 'k':
+        UNITS['temperature'][item]['convert'] = kelvin
+for item in UNITS['volume']:
+    if item == 'c':
+        UNITS['volume'][item]['convert'] = cups
+    if item == 'ci':
+        UNITS['volume'][item]['convert'] = cubicInches
+    if item == 'cf':
+        UNITS['volume'][item]['convert'] = cubicFeet
+    if item == 'g':
+        UNITS['volume'][item]['convert'] = gallons
+    if item == 'l':
+        UNITS['volume'][item]['convert'] = liters
+    if item == 'tbsp':
+        UNITS['volume'][item]['convert'] = tablespoons
+
 
 # main script
-val = askInput('Please enter a numerical value', True)
-print(celsius(val, 'fahrenheit'))
-
+validateStudent(
+    askInput('Please enter a numerical value', True),
+    askInput('Please enter a unit of measure', False, True),
+    askInput('Please enter the target unit of measure', False, True),
+    str(543.87)
+)
 
